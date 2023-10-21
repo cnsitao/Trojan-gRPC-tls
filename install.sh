eb085d17d66c2c461a70a78d6407476aec326479
Sitao <121932158+cnsitao@users.noreply.github.com>
2023-10-21 14:53:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -176,13 +176,6 @@ systemctl start xray.service
 systemctl enable xray.service
 
 #安装nginx
-apt install curl gnupg2 ca-certificates lsb-release debian-archive-keyring -y
-curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
-    | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
-gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
-http://nginx.org/packages/debian `lsb_release -cs` nginx" \
-    | tee /etc/apt/sources.list.d/nginx.list
 apt update
 apt install nginx -y
 nginx -v
